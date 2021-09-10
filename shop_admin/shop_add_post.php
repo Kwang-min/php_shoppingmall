159cd911cbdd3ae074d95b4fee0a2e2f55910387
Kwang-min <j2h2677@gmail.com>
2021-09-10 17:35:31 +0900

    상품등록 mysqli로 구현

--- shop_admin/shop_add_post.php
+++ shop_admin/shop_add_post.php
@@ -2,31 +2,23 @@
   include "lib.php";
 
   $conn = mysqli_connect('localhost', 'root', '111111', 'php_shoppingmall');
-  
 
   if ($_FILES['img']['tmp_name']) {
     move_uploaded_file($_FILES['img']['tmp_name'],__DIR__."/shop/{$_FILES['img']['name']}");
 
   }
   
-  // $query = "insert into shop_data(name, comment, price, memo, img)
-  //           values(
-  //             '{$_POST['name']}', '{$_POST['comment']}', 
-  //             '{$_POST['price']}', '{$_POST['memo']}', 
-  //             '{$_FILES['img']['name']}'
-  //           )";
+  $query = "insert into shop_data(name, comment, price, memo, img)
+            values(
+              '{$_POST['name']}', '{$_POST['comment']}', 
+              '{$_POST['price']}', '{$_POST['memo']}', 
+              '{$_FILES['img']['name']}'
+            )";
+
+  mysqli_query($conn,$query);
   
-  // $query = "INSERT INTO shop_data(name, comment, price, memo, img)
-  // VALUES
-  // (
-  //   'sdaf', 'dsfa', 
-  //   'dsfa', 'fdas', 
-  //   'sdf'
-  // )";
-  // $result = mysqli_query($conn, $query);
-  // var_dump($result);
   
 ?>
-<!-- <script>
+<script>
   location.href='shop_add.php'
-</script> -->
+</script>
